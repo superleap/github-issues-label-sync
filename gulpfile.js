75c70d54a5d1e28458dad50ab45f97bee956935a
TED Vortex <ted.vortex@gmail.com>
2016-07-07 07:00:18 +0100

    fix(gulp): added gulp prepublish
    previous commit added dependencies but not the gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+let exec = require('child_process').exec;
+let path = require('path');
+let gulp = require('gulp');
+let nsp = require('gulp-nsp');
+
+gulp.task('nsp', function (cb) {
+    nsp({package: path.resolve('package.json')}, cb);
+});
+
+gulp.task('bithound', function () {
+    if (!process.env.CI) {
+        return;
+    }
+
+    exec('bithound check git@github.com:superleap/github-issues-label-sync.git', (error, stdout, stderr) => {
+        console.log(stdout);
+        console.log(stderr);
+    });
+});
+
+gulp.task('prepublish', ['nsp', 'bithound']);
